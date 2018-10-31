eebc5f5df3358ef447ed04492b1f71d9b757e9d2
Santiago Castro <sacastro@umich.edu>
2018-10-31 13:26:11 -0400

    WIP

--- lqa_framework/dataset_readers/lifeqa.py
+++ lqa_framework/dataset_readers/lifeqa.py
@@ -4,11 +4,13 @@ from typing import Any, Dict, Iterable, List, Optional
 
 from allennlp.common.file_utils import cached_path
 from allennlp.data.dataset_readers.dataset_reader import DatasetReader
-from allennlp.data.fields import LabelField, TextField, ListField
+from allennlp.data.fields import ArrayField, LabelField, TextField, ListField
 from allennlp.data.instance import Instance
 from allennlp.data.tokenizers import Tokenizer, WordTokenizer
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+import h5py
 from overrides import overrides
+import torch
 
 logger = logging.getLogger(__name__)
 
@@ -18,46 +20,69 @@ class LqaDatasetReader(DatasetReader):
     """Reads a JSON file containing questions and answers, and creates a dataset suitable for QA. """
 
     def __init__(self, lazy: bool = False, tokenizer: Optional[Tokenizer] = None,
-                 token_indexers: Optional[Dict[str, TokenIndexer]] = None) -> None:
+                 token_indexers: Optional[Dict[str, TokenIndexer]] = None,
+                 load_video_features: Optional[bool] = False) -> None:
         super().__init__(lazy=lazy)
         self._tokenizer = tokenizer or WordTokenizer()
         self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
+        self.load_video_features = load_video_features
 
     @overrides
     def _read(self, file_path: str) -> Iterable[Instance]:
+        if self.load_video_features:
+            logger.info("Reading video features of instances")
+            features_file = h5py.File('data/features/LifeQA_RESNET_pool5.hdf5')
+
         with open(cached_path(file_path)) as data_file:
-            video_dict = json.load(data_file)
             logger.info("Reading instances in file at: %s", file_path)
+            video_dict = json.load(data_file)
             for video_id in video_dict:
-                questions = video_dict[video_id]['questions']
+                question_dicts = video_dict[video_id]['questions']
                 captions = video_dict[video_id]['captions']
-                for question in questions:
-                    question = question['question']
-                    answers = question['answers']
-                    correct_index = question['correct_index']
-                    yield self.text_to_instance(question, answers, correct_index, captions)
+
+                if self.load_video_features:
+                    # noinspection PyUnboundLocalVariable
+                    video_features = features_file[video_id].value
+                else:
+                    video_features = None
+
+                for question_dict in question_dicts:
+                    question_text = question_dict['question']
+                    answers = question_dict['answers']
+                    correct_index = question_dict['correct_index']
+                    yield self.text_to_instance(question_text, answers, correct_index, captions, video_features)
+
+        if self.load_video_features:
+            features_file.close()
 
     @overrides
     def text_to_instance(self, question: str, answers: List[str], correct_index: Optional[int] = None,
-                         captions: Optional[Dict[str, Any]] = None, unroll: Optional[bool] = True) -> Instance:
+                         captions: Optional[Dict[str, Any]] = None, video_features: Optional[torch.Tensor] = None,
+                         unroll: Optional[bool] = True) -> Instance:
         tokenized_question = self._tokenizer.tokenize(question)
         tokenized_answers = (self._tokenizer.tokenize(a) for a in answers)
 
         if captions:
             if unroll:
                 # noinspection PyTypeChecker
-                tokenized_captions = [self._tokenizer.tokenize(' '.join(c['transcript'] for c in captions))]
+                tokenized_captions = [self._tokenizer.tokenize(' '.join(caption['transcript'] for caption in captions))]
             else:
                 # noinspection PyTypeChecker
-                tokenized_captions = (self._tokenizer.tokenize(c['transcript']) for c in captions)
+                tokenized_captions = (self._tokenizer.tokenize(caption['transcript']) for caption in captions)
         else:
             tokenized_captions = [self._tokenizer.tokenize('')]
 
         question_field = TextField(tokenized_question, self._token_indexers)
         answers_field = ListField([TextField(answer, self._token_indexers) for answer in tokenized_answers])
-        captions_field = ListField([TextField(c, self._token_indexers) for c in tokenized_captions])
+        captions_field = ListField([TextField(caption, self._token_indexers) for caption in tokenized_captions])
+        video_features_field = ArrayField(video_features)
 
-        fields = {'question': question_field, 'answers': answers_field, 'captions': captions_field}
+        fields = {
+            'question': question_field,
+            'answers': answers_field,
+            'captions': captions_field,
+            'video_features': video_features_field,
+        }
         if correct_index is not None:
             fields['label'] = LabelField(str(correct_index))
 
--- lqa_framework/models/tgif_qa.py
+++ lqa_framework/models/tgif_qa.py
@@ -3,7 +3,7 @@ from typing import Dict, Optional
 from allennlp.data import Vocabulary
 from allennlp.models.model import Model
 from allennlp.modules import FeedForward, Seq2VecEncoder, TextFieldEmbedder, TimeDistributed
-from allennlp.nn import InitializerApplicator, RegularizerApplicator
+from allennlp.nn import InitializerApplicator, RegularizerApplicator, util
 from allennlp.training.metrics import CategoricalAccuracy
 from overrides import overrides
 import torch
@@ -26,13 +26,14 @@ class TgifQaClassifier(Model):
         self.classifier_feedforward = classifier_feedforward
 
         self.metrics = {'accuracy': CategoricalAccuracy()}
+        self.loss = torch.nn.HingeEmbeddingLoss()
 
         initializer(self)
 
     # noinspection PyUnresolvedReferences
     @overrides
     def forward(self, question: Dict[str, torch.LongTensor], answers: Dict[str, torch.LongTensor],
-                captions: Dict[str, torch.LongTensor],
+                captions: Dict[str, torch.LongTensor], video_features: torch.Tensor,
                 label: Optional[torch.LongTensor] = None) -> Dict[str, torch.Tensor]:
         """Does the forward pass.
 
@@ -41,17 +42,19 @@ class TgifQaClassifier(Model):
         question : Dict[str, Variable], required	The output of ``TextField.as_array()``.
         answers : Dict[str, Variable], required		The output of ``TextField.as_array()``.
         captions : Dict[str, Variable], required 	The output of ``TextField.as_array()``.
+        video_features : np.ndarray, required       The video features.
         label : Variable, optional (default = None)	A variable representing the label for each instance in the batch.
 
         Returns
         -------
         An output dictionary consisting of:
-        class_probabilities : torch.FloatTensor  FIXME
-            A tensor of shape ``(batch_size, num_classes)`` representing a distribution over the
-            label classes for each instance.
+        scores : torch.FloatTensor  FIXME
+            A tensor of shape ``(batch_size, num_classes)`` representing the scores for each answer.
         loss : torch.FloatTensor, optional
             A scalar loss to be optimised.
         """
+        encoded_video = self.video_encoder(video_features, [1] * len(video_features))
+
         embedded_question = self.text_field_embedder(question)
         question_mask = util.get_text_field_mask(question)
         encoded_question = self.question_encoder(embedded_question, question_mask)
@@ -60,11 +63,11 @@ class TgifQaClassifier(Model):
         answers_mask = util.get_text_field_mask(answers, num_wrapping_dims=1)
         encoded_answers = self.answers_encoder(embedded_answers, answers_mask)
 
-        scores = self.classifier_feedforward(...)
+        scores = self.classifier_feedforward(encoded_answers)
 
-        output_dict = {}
+        output_dict = {'scores': scores}
         if label is not None:
-            output_dict['loss'] = ...
+            output_dict['loss'] = self.loss(scores, labels)
             for metric in self.metrics.values():
                 metric(logits, label)
 
