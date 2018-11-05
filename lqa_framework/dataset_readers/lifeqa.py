325e5eba3539bfd3dc74e781eca06bf6e1f7405e
Santiago Castro <santi.1410@hotmail.com>
2018-11-05 01:22:18 -0500

    Fix allennlp TGIF-QA model issues

--- lqa_framework/dataset_readers/lifeqa.py
+++ lqa_framework/dataset_readers/lifeqa.py
@@ -9,8 +9,8 @@ from allennlp.data.instance import Instance
 from allennlp.data.tokenizers import Tokenizer, WordTokenizer
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 import h5py
+import numpy as np
 from overrides import overrides
-import torch
 
 logger = logging.getLogger(__name__)
 
@@ -21,11 +21,13 @@ class LqaDatasetReader(DatasetReader):
 
     def __init__(self, lazy: bool = False, tokenizer: Optional[Tokenizer] = None,
                  token_indexers: Optional[Dict[str, TokenIndexer]] = None,
-                 load_video_features: Optional[bool] = False) -> None:
+                 load_video_features: Optional[bool] = False,
+                 check_missing_video_features: Optional[bool] = True) -> None:
         super().__init__(lazy=lazy)
         self._tokenizer = tokenizer or WordTokenizer()
         self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
         self.load_video_features = load_video_features
+        self.check_missing_video_features = check_missing_video_features
 
     @overrides
     def _read(self, file_path: str) -> Iterable[Instance]:
@@ -37,27 +39,29 @@ class LqaDatasetReader(DatasetReader):
             logger.info("Reading instances in file at: %s", file_path)
             video_dict = json.load(data_file)
             for video_id in video_dict:
-                question_dicts = video_dict[video_id]['questions']
-                captions = video_dict[video_id]['captions']
+                # noinspection PyUnboundLocalVariable
+                if not self.load_video_features or self.check_missing_video_features or video_id in features_file:
+                    question_dicts = video_dict[video_id]['questions']
+                    captions = video_dict[video_id]['captions']
 
-                if self.load_video_features:
-                    # noinspection PyUnboundLocalVariable
-                    video_features = features_file[video_id].value
-                else:
-                    video_features = None
+                    if self.load_video_features:
+                        # noinspection PyUnboundLocalVariable
+                        video_features = features_file[video_id].value
+                    else:
+                        video_features = None
 
-                for question_dict in question_dicts:
-                    question_text = question_dict['question']
-                    answers = question_dict['answers']
-                    correct_index = question_dict['correct_index']
-                    yield self.text_to_instance(question_text, answers, correct_index, captions, video_features)
+                    for question_dict in question_dicts:
+                        question_text = question_dict['question']
+                        answers = question_dict['answers']
+                        correct_index = question_dict['correct_index']
+                        yield self.text_to_instance(question_text, answers, correct_index, captions, video_features)
 
         if self.load_video_features:
             features_file.close()
 
     @overrides
     def text_to_instance(self, question: str, answers: List[str], correct_index: Optional[int] = None,
-                         captions: Optional[Dict[str, Any]] = None, video_features: Optional[torch.Tensor] = None,
+                         captions: Optional[Dict[str, Any]] = None, video_features: Optional[np.ndarray] = None,
                          unroll: Optional[bool] = True) -> Instance:
         tokenized_question = self._tokenizer.tokenize(question)
         tokenized_answers = (self._tokenizer.tokenize(a) for a in answers)
@@ -75,14 +79,17 @@ class LqaDatasetReader(DatasetReader):
         question_field = TextField(tokenized_question, self._token_indexers)
         answers_field = ListField([TextField(answer, self._token_indexers) for answer in tokenized_answers])
         captions_field = ListField([TextField(caption, self._token_indexers) for caption in tokenized_captions])
-        video_features_field = ArrayField(video_features)
 
         fields = {
             'question': question_field,
             'answers': answers_field,
             'captions': captions_field,
-            'video_features': video_features_field,
         }
+
+        if video_features is not None:
+            fields['video_features'] = ArrayField(video_features)
+            fields['frame_count'] = ArrayField(np.asarray([len(video_features)], dtype=np.int32))
+
         if correct_index is not None:
             fields['label'] = LabelField(str(correct_index))
 
--- lqa_framework/models/tgif_qa.py
+++ lqa_framework/models/tgif_qa.py
@@ -5,8 +5,12 @@ from allennlp.models.model import Model
 from allennlp.modules import FeedForward, Seq2VecEncoder, TextFieldEmbedder, TimeDistributed
 from allennlp.nn import InitializerApplicator, RegularizerApplicator, util
 from allennlp.training.metrics import CategoricalAccuracy
+import numpy as np
 from overrides import overrides
 import torch
+import torch.nn.functional as F
+
+from .time_distributed_rnn import TimeDistributedRNN
 
 
 @Model.register('tgif_qa')
@@ -22,18 +26,17 @@ class TgifQaClassifier(Model):
 
         self.video_encoder = video_encoder
         self.question_encoder = question_encoder
-        self.answers_encoder = TimeDistributed(answers_encoder)
-        self.classifier_feedforward = classifier_feedforward
+        self.answers_encoder = TimeDistributedRNN(answers_encoder)
+        self.classifier_feedforward = TimeDistributed(classifier_feedforward)
 
         self.metrics = {'accuracy': CategoricalAccuracy()}
-        self.loss = torch.nn.HingeEmbeddingLoss()
+        self.loss = torch.nn.CrossEntropyLoss()  # TODO: HingeEmbeddingLoss()
 
         initializer(self)
 
-    # noinspection PyUnresolvedReferences
     @overrides
     def forward(self, question: Dict[str, torch.LongTensor], answers: Dict[str, torch.LongTensor],
-                captions: Dict[str, torch.LongTensor], video_features: torch.Tensor,
+                captions: Dict[str, torch.LongTensor], video_features: torch.Tensor, frame_count: torch.Tensor,
                 label: Optional[torch.LongTensor] = None) -> Dict[str, torch.Tensor]:
         """Does the forward pass.
 
@@ -42,38 +45,64 @@ class TgifQaClassifier(Model):
         question : Dict[str, Variable], required	The output of ``TextField.as_array()``.
         answers : Dict[str, Variable], required		The output of ``TextField.as_array()``.
         captions : Dict[str, Variable], required 	The output of ``TextField.as_array()``.
-        video_features : np.ndarray, required       The video features.
+        video_features : torch.Tensor, required     The video features.
+        frame_count : torch.Tensor, required        The frame count.
         label : Variable, optional (default = None)	A variable representing the label for each instance in the batch.
 
         Returns
         -------
         An output dictionary consisting of:
-        scores : torch.FloatTensor  FIXME
+        scores : torch.FloatTensor
             A tensor of shape ``(batch_size, num_classes)`` representing the scores for each answer.
         loss : torch.FloatTensor, optional
             A scalar loss to be optimised.
         """
-        encoded_video = self.video_encoder(video_features, mask=None)  # FIXME: should mask because it's a batch
+        batch_size = len(video_features)
+        # noinspection PyProtectedMember
+        num_layers = self.video_encoder._module.num_layers
+        num_answers = answers['tokens'].shape[1]  # This supposes a fixed number of answers.
+
+        frame_count = frame_count.reshape(-1)  # We have to do this because ArrayFields do not support scalars.
+
+        video_features_mask = util.get_mask_from_sequence_lengths(frame_count, int(max(frame_count).item()))
+        encoded_video = self.video_encoder(video_features, mask=video_features_mask)
+        encoded_video = encoded_video.reshape(num_layers, batch_size, -1)
 
         embedded_question = self.text_field_embedder(question)
         question_mask = util.get_text_field_mask(question)
+        # Passing the hidden state with LSTM doesn't work. This is because allennlp seq2vec wrapper only keeps h
+        # (the hidden state) but not the cell state c. A workaround should be done if we want to chain LSTMs.
         encoded_question = self.question_encoder(embedded_question, question_mask, hidden_state=encoded_video)
+        encoded_question = encoded_question.reshape(num_layers, batch_size, 1, -1)\
+            .expand(-1, -1, num_answers, -1)
 
         embedded_answers = self.text_field_embedder(answers)
         answers_mask = util.get_text_field_mask(answers, num_wrapping_dims=1)
         encoded_answers = self.answers_encoder(embedded_answers, answers_mask, hidden_state=encoded_question)
 
-        scores = self.classifier_feedforward(encoded_answers)
+        scores = self.classifier_feedforward(encoded_answers).squeeze(2)
 
         output_dict = {'scores': scores}
+
         if label is not None:
-            output_dict['loss'] = self.loss(scores, labels)
+            output_dict['loss'] = self.loss(scores, label)
             for metric in self.metrics.values():
-                metric(logits, label)
+                metric(scores, label)
 
         return output_dict
 
-    # TODO: decode?
+    @overrides
+    def decode(self, output_dict: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        """Does a simple argmax over the class probabilities, converts indices to string labels, and adds a ``'label'``
+        key to the dictionary with the result. """
+        class_probabilities = F.softmax(output_dict['scores'], dim=-1)
+        output_dict['class_probabilities'] = class_probabilities
+
+        predictions = class_probabilities.cpu().data.numpy()
+        argmax_indices = np.argmax(predictions, axis=-1)
+        output_dict['label'] = torch.Tensor([self.vocab.get_token_from_index(x, namespace='labels')
+                                             for x in argmax_indices])
+        return output_dict
 
     @overrides
     def get_metrics(self, reset: Optional[bool] = False) -> Dict[str, float]:
